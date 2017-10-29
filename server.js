15c6a513b0568748d25ec6d27737ab687c539358
Michael Ngarimu <ngarimu@gmail.com>
2017-10-28 18:36:41 -0700

    Adding webhook token code for Azure

--- server.js
+++ server.js
@@ -19,4 +19,14 @@ app.get('/', function (req, res) {
     res.send('This is my bot');
 });
 
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'gyantchatbot_verify_token') {
+	res.status(200).send(req.query['hub.challenge']);
+    }
+    else
+    {
+	res.status(403).send('Invalid verify token');
+    }
+);
+
 app.listen((process.env.PORT || 8080));
