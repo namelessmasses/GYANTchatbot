24405d796e09edcf59d4ee2440467db357bd0c9f
Michael Ngarimu <ngarimu@gmail.com>
2017-10-28 19:29:24 -0700

    Found syntax error

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello, world!');
     
-}).listen(process.env.PORT || 8080);
+// }).listen(process.env.PORT || 8080);
 
-// var express = require('express')
-// var bodyParser = require('body-parser')
-// var request = require('request')
-// var app = express()
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// app.get('/', function (req, res) {
-//     res.send('This is my bot');
-// });
+app.get('/', function (req, res) {
+    res.send('This is my bot');
+});
 
-// app.get('/webhook', function (req, res) {
-//     if (req.query['hub.verify_token'] === 'gyantchatbot_verify_token') {
-// 	res.status(200).send(req.query['hub.challenge']);
-//     }
-//     else
-//     {
-// 	res.status(403).send('Invalid verify token');
-//     }
-// );
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'gyantchatbot_verify_token')
+    {
+	res.status(200).send(req.query['hub.challenge']);
+    }
+    else
+    {
+	res.status(403).send('Invalid verify token');
+    }
+});
 
-// app.listen((process.env.PORT || 8080));
+app.listen((process.env.PORT || 8080));
