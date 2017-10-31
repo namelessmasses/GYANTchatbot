3d98d5634880be9bc7b1d663c3253181f3280f92
Michael Ngarimu <ngarimu@gmail.com>
2017-10-30 22:17:42 -0700

    - server.js: use strict

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var moment = require('moment')
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+'use strict';
+var moment = require('moment');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 function talkToGYANT(output)
 {
